7b96699bdffd59150a2fb54b55a06e9414ae2792
kuzeeeyk <kuzeeeyk@gmail.com>
2021-08-22 20:17:50 +0300

    Made ui of profiler.

--- src/renderer.js
+++ src/renderer.js
@@ -1,12 +1,10 @@
 const { ipcRenderer } = require('electron')
 const { getVersions, filterVersions } = require("./scripts/versions.js")
 const launchMinecraft = require("./scripts/launcher.js");
-var filteredVersions = new Array();
-var versions;
-getVersions().then(data => {
-    versions = data;
-    updateVersions("release", true);
-});
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const profilesAdapter = new FileSync("profiles.json");
+
 require("./scripts/ipc")
 
 var options = {
@@ -14,29 +12,26 @@ var options = {
         release: false,    // releases like 1.17, 1.8 etc.
         snapshot: false,  // snapshots like 21w16a etc.
         old_alpha: false  // releases like release-candite, infdev etc.
-    },
-    lang: process.env.lang // TO-DO // Add language support
+    }
 }
 
-setTimeout(console.clear, 500)
-
-/**
- * @param {String} option 
- * @param {Boolean} boolean 
- */
-function updateVersions(option, boolean) {
-    if (!Object.keys(options.versions).includes(option))
-        return new Error("Invalid option inputted");
-    if (typeof boolean != "boolean")
-        return new Error("Parameter 'boolean' must be Boolean");
-    options.versions[option] = boolean;
-    filteredVersions = filterVersions(versions, options.versions);
-    // document.getElementById("version").innerHTML = filteredVersions.length > 0 ? filteredVersions.map(ver => {
-        // return `<option value="${ver}" data-type="${ver.type}">${ver.id}</option>`
-    // }) : `<option value="false" data-type="info">Select version type checkbox</option>`
-}
-
-function launch() {
-    const selectedVersion = document.getElementById("version").value;
-    console.log(launchMinecraft(selectedVersion, "2G"))
-}
+async function loadProfiles() {
+    var profilesEl = document.getElementById("profiles");
+    var profiles = low(profilesAdapter);
+    profilesEl.innerHTML == "";
+    (await profiles.get("versions").write()).forEach(profile => {
+        console.log(profile)
+        profilesEl.innerHTML += `
+        <div class="profile">
+            <div class="profileDetails">
+                <span class="profileName">${profile.name}</span>
+                <span class="profileIdentifier">${profile.version}</span>
+            </div>
+            <div class="profileActions">
+                <span class="profileAction material-icons btn sidebarItem" onclick="deleteProfile(${profile.id});">delete</span>
+                <span class="profileAction material-icons btn sidebarItem" onclick="moreInfoOfProfile(${profile.id});">more_horiz</span>
+            </div>
+      </div > `
+    });
+};
+loadProfiles();
--- src/scripts/profiler.js
+++ src/scripts/profiler.js
@@ -3,9 +3,11 @@ const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync("profiles.json")
 
 module.exports = {
-    addVersion: function (version = "1.17.1", allocatedMemory = "2G", width = 820, height = 480, name = version) {
+    addVersion: function (version = "1.17.1", allocatedMemory = "2G", width = 820, height = 480, name = "Unnamed") {
         const versions = low(adapter);
         const profile = {
+            id: Date.now(),
+            name: name,
             version: version,
             allocatedMemory: allocatedMemory,
             width: width,
@@ -13,11 +15,9 @@ module.exports = {
         };
         versions.defaults({ versions: [] })
             .write();
-        console.log(
-            versions.get("versions")
+        versions.get("versions")
             .push(profile)
             .write()
-        )
-        console.log(name)
+        loadProfiles();
     }
 }
