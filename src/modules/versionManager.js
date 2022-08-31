2223e324ca91279085d33da27ae4fd5a5a53d728
Kuzey Kurtuluş <kuzeeeyk@gmail.com>
2022-08-31 18:53:05 +0300

    Now you can launch the app without installing MC🥴

--- src/modules/versionManager.js
+++ src/modules/versionManager.js
@@ -39,8 +39,10 @@ class VersionManager {
   }
 
   async init() {
-    if (fs.existsSync(versionsDir) || fs.existsSync(minecraftDir))
+    if (fs.existsSync(versionsDir) || fs.existsSync(minecraftDir)) {
       this.doesExist = false;
+      fs.mkdirSync(versionsDir);
+    }
     if (db.get("profiles").size().value() == 0) {
       this.addProfile({
         version: this.latest.release,
@@ -74,29 +76,31 @@ class VersionManager {
 
   async loadVersions() {
     let apiVersions = await this.getVersionFromAPI();
-    let versionFolders = fs.readdirSync(versionsDir);
     let versions = [];
-    versionFolders.forEach((versionFolder) => {
-      let stats = fs.statSync(path.join(versionsDir, versionFolder));
-      if (versionFolder.startsWith("."))
-        return;
-      if (!stats.isDirectory())
-        return;
-      let versionDir = fs.readdirSync(path.join(versionsDir, versionFolder));
-      if (!(versionDir.includes(versionFolder + ".json") && versionDir.includes(versionFolder + ".jar")))
-        return;
-      let versionData = JSON.parse(fs.readFileSync(path.join(versionsDir, versionFolder, versionFolder + ".json")));
-      if (apiVersions.versions.map(v => v.id).includes(versionFolder))
-        return;
-      versions.push({
-        id: versionData.id,
-        java: versionData.javaVersion ? versionData.javaVersion.majorVersion : 16,
-        releaseTime: versionData.inheritsFrom ? apiVersions.versions.filter(version => version.id ==  versionData.inheritsFrom)[0]?.releaseTime : versionData.releaseTime,
-        actualReleaseTime: versionData.releaseTime,
-        actualVersion: apiVersions.versions.find(v => v?.id == versionData?.inheritsFrom) || undefined,
-        type: versionData.type
+    if (this.doesExist) {
+      let versionFolders = fs.readdirSync(versionsDir);
+      versionFolders.forEach((versionFolder) => {
+        let stats = fs.statSync(path.join(versionsDir, versionFolder));
+        if (versionFolder.startsWith("."))
+          return;
+        if (!stats.isDirectory())
+          return;
+        let versionDir = fs.readdirSync(path.join(versionsDir, versionFolder));
+        if (!(versionDir.includes(versionFolder + ".json") && versionDir.includes(versionFolder + ".jar")))
+          return;
+        let versionData = JSON.parse(fs.readFileSync(path.join(versionsDir, versionFolder, versionFolder + ".json")));
+        if (apiVersions.versions.map(v => v.id).includes(versionFolder))
+          return;
+        versions.push({
+          id: versionData.id,
+          java: versionData.javaVersion ? versionData.javaVersion.majorVersion : 16,
+          releaseTime: versionData.inheritsFrom ? apiVersions.versions.filter(version => version.id ==  versionData.inheritsFrom)[0]?.releaseTime : versionData.releaseTime,
+          actualReleaseTime: versionData.releaseTime,
+          actualVersion: apiVersions.versions.find(v => v?.id == versionData?.inheritsFrom) || undefined,
+          type: versionData.type
+        });
       });
-    });
+    }
     versions = versions.concat(apiVersions.versions.map(version => {
       return {
         id: version.id,
